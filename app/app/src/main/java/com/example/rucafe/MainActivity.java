c3f1932b603793a912b4b9a010de8518a4c8f89a
linst <linstephanie89@gmail.com>
2023-04-16 14:28:53 -0400

    created new project for android app

---
+++ app/app/src/main/java/com/example/rucafe/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.rucafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * hey
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
