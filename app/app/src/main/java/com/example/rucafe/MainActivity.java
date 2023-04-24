c7d0d2aa7f1b33698e04b8eec38273619af6b6f5
linst <linstephanie89@gmail.com>
2023-04-23 22:46:04 -0400

    added rucafe codes

--- app/app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/app/src/main/java/com/example/rucafe/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.rucafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * hey
  */
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private ImageButton donutButton;
+    private ImageButton coffeeButton;
+    private ImageButton cartButton;
+    private ImageButton historyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void donutClick(View view){
+        Toast.makeText(this, "donut", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, donut_activity.class);
+        startActivity(intent);
+    }
+    public void coffeeClick(View view){
+        Toast.makeText(this, "coffee", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, activity_coffee.class);
+        startActivity(intent);
+    }
+    public void cartClick(View view){
+        Toast.makeText(this, "cart", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, activity_cart.class);
+        startActivity(intent);
+    }
+    public void historyClick(View view){
+        Toast.makeText(this, "history", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, activity_history.class);
+        startActivity(intent);
     }
 }
